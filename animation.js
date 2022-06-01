7dbcf1ab4283e22cfb3aa382d10aa221f665bf6e
Chase <bcreynolds2@gmail.com>
2022-06-01 19:01:30 -0400

    Move setup logic into helper methods

--- animation.js
+++ animation.js
@@ -1,36 +1,9 @@
 function setup() {
   createCanvas(windowWidth, windowHeight);
   controls_init();
-
-  let vec = createVector(0, 300);
-  
-  vec.rotate(2 * PI / 3);
-  input_A = new PointObj(vec.x, vec.y, 'A');
-  inputPoints.push(input_A);
-
-  vec.rotate(2 * PI / 3);
-  input_B = new PointObj(vec.x, vec.y, 'B');
-  inputPoints.push(input_B);
-
-  vec.rotate(2 * PI / 3);
-  input_C = new PointObj(vec.x, vec.y, 'C');
-  inputPoints.push(input_C);
-
-  AB = new SegmentObj(input_A, input_B);
-  BC = new SegmentObj(input_B, input_C);
-  ABC = new TriangleObj(AB, BC);
-  validTriangles.push(ABC)
-
-  for (let i = 0; i < n; i++) {
-    let newPoint = new PointObj(random(-windowWidth/2, windowWidth/2), random(-windowHeight/2, windowHeight/2));
-    inputPoints.push(newPoint);
-    validTriangles.forEach(t => {
-      if (t.pointIsInCircumcircle(newPoint)) {
-        console.log(true);
-      }
-    })
-
-  }
+  input_points_init();
+  valid_triangles_init();
+  construct_delaunay();
 }
   
 function draw() {
@@ -76,10 +49,45 @@ function draw() {
     inputPoints.forEach(p => p.show())
   }
 
-  // if(controls.showLabels) {
-  //   input_A.showLabel();
-  //   input_B.showLabel();
-  //   input_C.showLabel();
-  // }
+  if(controls.showLabels) {
+    input_A.showLabel();
+    input_B.showLabel();
+    input_C.showLabel();
+  }
   
+}
+
+function input_points_init() {
+  let vec = createVector(0, 300);
+  
+  vec.rotate(2 * PI / 3);
+  input_A = new PointObj(vec.x, vec.y, 'A');
+  inputPoints.push(input_A);
+
+  vec.rotate(2 * PI / 3);
+  input_B = new PointObj(vec.x, vec.y, 'B');
+  inputPoints.push(input_B);
+
+  vec.rotate(2 * PI / 3);
+  input_C = new PointObj(vec.x, vec.y, 'C');
+  inputPoints.push(input_C);
+}
+
+function valid_triangles_init() {
+  AB = new SegmentObj(input_A, input_B);
+  BC = new SegmentObj(input_B, input_C);
+  ABC = new TriangleObj(AB, BC);
+  validTriangles.push(ABC);
+}
+
+function construct_delaunay() {
+  for (let i = 0; i < n; i++) {
+    let newPoint = new PointObj(random(-windowWidth/2, windowWidth/2), random(-windowHeight/2, windowHeight/2));
+    inputPoints.push(newPoint);
+    validTriangles.forEach(t => {
+      if (t.pointIsInCircumcircle(newPoint)) {
+        console.log(true);
+      }
+    })
+  }
 }
